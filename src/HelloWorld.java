2cf6d2e9e18d35daf7fa06d82ae4a8d4ca8e9556
omerrRahsan <farukkoc91@gmail.com>
2022-01-11 10:56:52 -0500

    work 4 is done

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,9 @@ public class HelloWorld {
     // omer faruk rahsan
     // neva rose rahsan
     //Shannon ryan rahsan
+    public static void main(String[] args) {
+        System.out.println("Omer");
+    }
 
 
 
